5c845e708de75ef2e89fdaa56dd2516bd01e69aa
tiagocaus <sac@hostcia.net>
2021-11-21 12:13:15 -0300

    update

--- index.js
+++ index.js
@@ -9,7 +9,20 @@ require('dotenv').config();
 
 var app = express();
 
-app.use(cors())
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain)
 // app.use(timeout(120000));
 // app.use(haltOnTimedout);
 app.use(express.json({
