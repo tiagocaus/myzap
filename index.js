bd3c99b831a074dea7cd6039a7812b6d9534f71e
tiagocaus <sac@hostcia.net>
2021-11-21 16:29:46 -0300

    update

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header('Access-Control-Allow-Headers', 'application/json');
 
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
